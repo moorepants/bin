8eedc7ea133114577d0731824fa693245726e788
Jason K. Moore <moorepants@gmail.com>
2020-04-21 16:23:02 -0700

    Added update and clean script.

---
+++ update_and_clean.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y autoremove
+sudo apt -y clean
+/home/moorepants/miniconda3/bin/conda update --all
+/home/moorepants/miniconda3/bin/conda clean --all
