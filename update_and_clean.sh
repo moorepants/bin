be11158db1780390186f5466d84fe3124de181f3
Jason K. Moore <moorepants@gmail.com>
2020-06-11 12:18:59 -0700

    Make conda command work on all computers.

--- update_and_clean.sh
+++ update_and_clean.sh
@@ -2,5 +2,5 @@ sudo apt update
 sudo apt -y upgrade
 sudo apt -y autoremove
 sudo apt -y clean
-/home/moorepants/miniconda3/bin/conda update --all
-/home/moorepants/miniconda3/bin/conda clean --all
+conda update --all
+conda clean --all
