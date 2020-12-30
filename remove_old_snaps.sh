72e5eec085f9673e7db9ee8517f84e2ccb27816a
Jason K. Moore <moorepants@gmail.com>
2020-12-30 12:40:23 +0100

    Added script to purge old snaps.

---
+++ remove_old_snaps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# From: https://superuser.com/questions/1310825/how-to-remove-old-version-of-installed-snaps
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+
+LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
