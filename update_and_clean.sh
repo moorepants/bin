273e3992ff60303cfcf6b5b8cfcbc30605db8b4f
Jason K. Moore <moorepants@gmail.com>
2024-04-22 21:04:44 +0200

    Update and remove old snaps.

--- update_and_clean.sh
+++ update_and_clean.sh
@@ -5,6 +5,8 @@ sudo apt -y upgrade
 sudo apt -y autoremove
 sudo apt -y autoclean
 sudo apt -y clean
+# TODO : The deb-get commands continue to run in the background after the conda
+# commands start. Figure out how to prevent this.
 # zoom
 # install deb-get with instructions here https://github.com/wimpysworld/deb-get
 if [ -d "/opt/zoom" ]
@@ -14,6 +16,9 @@ then
 else
   deb-get install zoom
 fi
+# snap
+sudo snap refresh
+sudo bash $HOME/bin/remove_old_snaps.sh
 # conda
 if [ -d "$HOME/miniconda" ]
 then
